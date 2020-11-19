89cb1c53aa54e38a626020422a25fee96d548182
Pranav Ravi <57525557+Pranav-Ravi@users.noreply.github.com>
2020-11-20 00:04:08 +0530

    firestore database from firebase has been setup
    firestore database from firebase has been setup in firebase.js file

---
+++ qikchat-messenger/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCPYWp-1WXnIylCnd4nc9kSEndtDVM8wB4",
+    authDomain: "qikchat-messenger.firebaseapp.com",
+    databaseURL: "https://qikchat-messenger.firebaseio.com",
+    projectId: "qikchat-messenger",
+    storageBucket: "qikchat-messenger.appspot.com",
+    messagingSenderId: "196321936161",
+    appId: "1:196321936161:web:996aff6b13afff9aeb3460",
+    measurementId: "G-WJ92T09ZHB"
+  };
+
+  const db = firebaseApp.fireStore();
+
+  export default db;
